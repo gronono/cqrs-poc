a4fbadf8538a94ccd649bc58833930e837d47cc3
Arnaud Brunet <arnaud.brunet+github@gmail.com>
2016-09-02 01:45:06 +1100

    spring boot

--- src/main/java/poc/demo/DemoApplication.java
+++ src/main/java/poc/demo/DemoApplication.java
@@ -1,5 +1,12 @@
 package poc.demo;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 import poc.cqrs.command.CommandBus;
 import poc.cqrs.command.CommandDispatcher;
 import poc.cqrs.command.CommandHandler;
@@ -7,22 +14,22 @@ import poc.cqrs.command.InvalidCommandException;
 import poc.cqrs.command.impl.DefaultCommandBus;
 import poc.cqrs.command.impl.DefaultCommandDispatcher;
 import poc.demo.product.command.CreateProductCommand;
-import poc.demo.product.command.CreateProductHandler;
 
-import java.util.Arrays;
-import java.util.List;
-
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) throws InvalidCommandException {
-		CommandBus commandBus = createCommandBus(new CreateProductHandler());
-		
-		commandBus.send(new CreateProductCommand("Produit 1"));
+		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	private static CommandBus createCommandBus(CommandHandler<?> ... handlers) {
-		List<CommandHandler<?>> commandHandlers = Arrays.asList(handlers);
-		CommandDispatcher commandDispatcher = new DefaultCommandDispatcher(commandHandlers);
+	@Bean
+	public CommandLineRunner run(CommandBus commandBus) {
+		return (args) -> commandBus.send(new CreateProductCommand("Produit 1")); 
+	}
+	
+	@Bean
+	public CommandBus createCommandBus(List<CommandHandler<?>> handlers) {
+		CommandDispatcher commandDispatcher = new DefaultCommandDispatcher(handlers);
 		CommandBus commandBus = new DefaultCommandBus(commandDispatcher);
 
 		return commandBus;
--- src/main/java/poc/demo/product/command/CreateProductHandler.java
+++ src/main/java/poc/demo/product/command/CreateProductHandler.java
@@ -1,15 +1,17 @@
 package poc.demo.product.command;
 
+import org.springframework.stereotype.Service;
+
 import poc.cqrs.command.CommandHandler;
 import poc.cqrs.command.HandlerFor;
 import poc.cqrs.command.InvalidCommandException;
 
+@Service
 @HandlerFor(CreateProductCommand.class)
 public class CreateProductHandler implements CommandHandler<CreateProductCommand> {
 
 	@Override
 	public void handle(CreateProductCommand command) throws InvalidCommandException {
-		System.out.println("Création du produit " + command.getName());
+		System.err.println("Création du produit " + command.getName());
 	}
-
 }
